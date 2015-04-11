cffaed665b6e6188f503acc80e2375d6b73f07bc
Alexandre Erich Sebastien Georges <erichaitian97@gmail.com>
2015-04-10 21:08:19 -0400

    Hospital entries #2 and migrations

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ class Doctor(models.Model):
 	
 class Hospital(models.Model):
 	nom = models.CharField(max_length=200)
+	quartier = models.CharField(max_length=75)
 	adresse = models.TextField(null=True)
 	email = models.EmailField(max_length=254)
 	phone_number = models.CharField(max_length=16)
