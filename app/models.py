92ebd0d35aa2d1e169651a5091c18adf0eb982cb
Alexandre Erich Sebastien Georges <erichaitian97@gmail.com>
2015-06-19 16:15:06 -0400

    Graphical bug and putting phone number link
    Little graphical bug I never seemed to notice 😁😇😇😇😇
    Making direct link phone number work

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Doctor(models.Model):
 	specialite = models.CharField(max_length=50)
 	adresse = models.TextField(null=True)
 	email = models.EmailField(max_length=254)
-	phone_number = models.CharField(max_length=16)
+	phone_number1 = models.CharField(max_length=16)
 	anecdote = models.TextField(null=True)    
 	
 class Hospital(models.Model):
@@ -18,5 +18,6 @@ class Hospital(models.Model):
 	quartier = models.CharField(max_length=75)
 	adresse = models.TextField(null=True)
 	email = models.EmailField(max_length=254)
-	phone_number = models.CharField(max_length=16)
+	phone_number1 = models.CharField(max_length=16)
+	phone_number2 = models.CharField(max_length=16)
 	specialite = models.CharField(max_length=50)
